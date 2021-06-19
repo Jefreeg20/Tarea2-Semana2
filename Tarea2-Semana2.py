a20ecac6a9b14c408f2b10e13600943062155826
jefreeg20 <gjefree@gmail.com>
2021-06-19 14:43:10 -0600

    first commit

---
+++ Tarea2-Semana2.py
@@ -0,0 +1,10 @@
+print ("**************************")
+print ("*       Area Circulo     *")
+print ("**************************")
+pi = 3.14
+
+radio = int(input("Ingresa el valor del radio: "))
+area = (radio ** 2) * pi
+
+print ("[El area es ]",  area)
+print ("Muchas Gracias por usar el programa.")
